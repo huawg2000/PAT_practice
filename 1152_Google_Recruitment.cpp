8fc5d33ecb3335bb9d342b5a8430e54c7316bb26
hwg <1162661100@qq.com>
2021-03-02 19:24:46 +0800

    1152 柳婼大神的代码

--- 1152_Google_Recruitment.cpp
+++ 1152_Google_Recruitment.cpp
@@ -1,30 +1,24 @@
-#include"iostream"
-#include"string"
+#include <iostream>
+#include <string>
 using namespace std;
-
 bool isPrime(int n) {
-  if (n == 0 || n == 1) return false;
-  for(int i = 2;i*i < n;i++) {
-    if(n % i == 0)return false;
-  }
-  return true;
+    if (n == 0 || n == 1) return false;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0) return false;
+    return true;
 }
-
-int main(int argc, char const *argv[]) {
-  string num;
-  int len,k;
-  cin>>len>>k;
-  cin>>num;
-
-  for(int i = 0;i < len - k;i++) {
-    string s = num.substr(i,k);
-    int t = stoi(s);
-    if(isPrime(t)) {
-      cout<<s;
-      return 0;
+int main() {
+    int l, k;
+    string s;
+    cin >> l >> k >> s;
+    for (int i = 0; i <= l - k; i++) {
+        string t = s.substr(i, k);
+        int num = stoi(t);
+        if (isPrime(num)) {
+            cout << t;
+            return 0;
+        }
     }
-  }
-  cout<<"404\n";
-
-  return 0;
+    cout << "404\n";
+    return 0;
 }
